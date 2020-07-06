1a659858293818de0ad1e05fbda19e8bb6ec1f66
jbb9229 <jbb9229@gmail.com>
2020-07-07 00:19:18 +0900

    Project First Setting & add Dependencies

---
+++ src/main/java/com/socialembed/jbbapi/JbbApiApplication.java
@@ -0,0 +1,13 @@
+package com.socialembed.jbbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbbApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbbApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/socialembed/jbbapi/JbbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialembed.jbbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbbApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
